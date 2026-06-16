20af507121878e2b9207a8a45d39de45ffd10d0b
lochani <lochaniketipearachchi@gmail.com>
2026-06-16 18:44:07 +0530

    comment

--- app/Http/Controllers/FloodChartController.php
+++ app/Http/Controllers/FloodChartController.php
@@ -75,6 +75,7 @@ class FloodChartController extends Controller
     }
 
     // Get all rows from that batch ordered by forecast_time
+    //comment
     $rows = DB::table('predictions')
         ->where('station_name', $station)
         ->where('created_at', $latestBatch)
